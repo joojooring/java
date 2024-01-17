4df761e4ed94c2853dc7fc3c799fee1de650bb2e
joojooring <ghkdwnfl0557@naver.com>
2024-01-17 22:19:07 +0900

    구구단

---
+++ src/practice/GooGooDan.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class GooGooDan {
+    public static void main(String[] args) {
+        System.out.println("==구구단==");
+        for(int i=1; i<10; i++){
+            System.out.println("1 x "+ i +" = "+ 1*i );
+        }
+        System.out.println("2단");
+
+        for(int i=1; i<10; i++){
+            System.out.println("2 x "+ i +" = "+ 2*i );
+        }
+    }
+}
